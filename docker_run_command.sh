5b9f558fb3548f81422f93c7bb08e022c61fa14d
Hoo Chang Shin <hshin@nvidia.com>
2018-04-19 04:34:08 -0700

    minor change to the previous.

--- docker_run_command.sh
+++ docker_run_command.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -v /datasets/dsb/unzipped:/datasets -v DSB2017-1:/home/DSB2017-1 -it --ipc=host -p 8888:8888 nvcr.io/nvidian/sae/ct_lung_nodule /bin/bash
+nvidia-docker run -v /datasets/dsb/unzipped:/datasets -v /tmp/DSB2017-1:/home/DSB2017-1 -it --ipc=host -p 8888:8888 nvcr.io/nvidian/sae/ct_lung_nodule /bin/bash
